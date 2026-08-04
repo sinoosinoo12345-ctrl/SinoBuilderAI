b231ceb66263bd4ad6457292e7d5154d78c1e1de
sinoosinoo12345 <sinoosinoo12345@gmail.com>
2026-08-05 01:21:20 +0300

    Fix backend IP

--- sino_builder_app/lib/pages/build_page.dart
+++ sino_builder_app/lib/pages/build_page.dart
@@ -29,7 +29,7 @@ class _BuildPageState extends State<BuildPage> {
 
     try {
       final response = await http.post(
-        Uri.parse("http://127.0.0.1:8000/build"),
+        Uri.parse("http://192.168.1.112:8000/build"),
         headers: {
           "Content-Type": "application/json",
         },
